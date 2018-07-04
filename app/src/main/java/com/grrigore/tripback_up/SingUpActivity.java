312f47f20e3bd8a1085e8daa9a2f02cd03529454
grrigore <cristi9627@yahoo.com>
2018-07-04 15:34:17 +0300

    firebase sign up

--- app/src/main/java/com/grrigore/tripback_up/SingUpActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/SingUpActivity.java
@@ -1,10 +1,20 @@
 package com.grrigore.tripback_up;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +29,9 @@ public class SingUpActivity extends AppCompatActivity {
     @BindView(R.id.tvLogin)
     TextView tvLogin;
 
+    private Context context = getApplicationContext();
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +39,33 @@ public class SingUpActivity extends AppCompatActivity {
 
         //bind views with butterknife
         ButterKnife.bind(this);
+
+        //get firebase auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void signup(View view) {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
 
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(context, getString(R.string.no_username), Toast.LENGTH_LONG).show();
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(context, getString(R.string.no_password), Toast.LENGTH_LONG).show();
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(SingUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(context, getString(R.string.create_account_error), Toast.LENGTH_LONG).show();
+                } else {
+                    startActivity(new Intent(SingUpActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        });
     }
 }
