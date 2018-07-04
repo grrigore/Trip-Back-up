3ac802627375b56b6f4924947e83fe1b0f5dfa5d
grrigore <cristi9627@yahoo.com>
2018-07-04 14:55:45 +0300

    added main activity class resources

--- app/src/main/java/com/grrigore/tripback_up/MainActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/MainActivity.java
@@ -1,13 +1,28 @@
 package com.grrigore.tripback_up;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etUsername)
+    EditText etUsername;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+    @BindView(R.id.tvCreateAccount)
+    TextView tvCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
     }
 }
