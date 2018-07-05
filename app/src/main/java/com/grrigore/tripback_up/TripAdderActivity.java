548d9522487523dca48549e1f82a5ff8a8d96152
grrigore <cristi9627@yahoo.com>
2018-07-05 15:02:16 +0300

    layout polish + added on click

--- app/src/main/java/com/grrigore/tripback_up/TripAdderActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/TripAdderActivity.java
@@ -2,6 +2,7 @@ package com.grrigore.tripback_up;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TripAdderActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class TripAdderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_adder);
+    }
+
+    public void addPlace(View view) {
+    }
+
+    public void addMedia(View view) {
     }
 }
