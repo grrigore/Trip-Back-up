4363988e49f3b17bb7d62fb6614577297883d82c
grrigore <cristi9627@yahoo.com>
2018-07-05 15:40:53 +0300

    menu options

--- app/src/main/java/com/grrigore/tripback_up/TripListActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/TripListActivity.java
@@ -1,7 +1,10 @@
 package com.grrigore.tripback_up;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TripListActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class TripListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.recentTrips:
+                //todo recent trip selection
+                return true;
+            case R.id.pastTrips:
+                //todo past trip selection
+                return true;
+            case R.id.favTrips:
+                //todo fav trip selection
+                return true;
+            case R.id.addTrip:
+                startActivity(new Intent(this, TripAdderActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
