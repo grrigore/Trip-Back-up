48aa88ec241d1a9827f375fd0c03b5d001293da2
grrigore <cristi9627@yahoo.com>
2018-07-05 15:15:26 +0300

    layout implementation

--- app/src/main/java/com/grrigore/tripback_up/TripAdderActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/TripAdderActivity.java
@@ -1,15 +1,30 @@
 package com.grrigore.tripback_up;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TripAdderActivity extends AppCompatActivity {
+
+    @BindView(R.id.etTitle)
+    EditText etTitle;
+    @BindView(R.id.etDescription)
+    EditText etDescription;
+    @BindView(R.id.gvMedia)
+    GridView gvMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_adder);
+
+        //bind views
+        ButterKnife.bind(this);
     }
 
     public void addPlace(View view) {
