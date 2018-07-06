f78f9b4619cc1bba7ab05aa601f15d5920bba519
grrigore <cristi9627@yahoo.com>
2018-07-06 15:36:36 +0300

    added toast util

---
+++ app/src/main/java/com/grrigore/tripback_up/utils/ToastUtil.java
@@ -0,0 +1,11 @@
+package com.grrigore.tripback_up.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    public static void showToast(String text, Context context) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+}
