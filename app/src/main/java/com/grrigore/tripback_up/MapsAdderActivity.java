889ad8f4a84f7b1f812ce90dba93ac402361c17c
grrigore <cristi9627@yahoo.com>
2018-07-07 13:24:16 +0300

    add marker funct

--- app/src/main/java/com/grrigore/tripback_up/MapsAdderActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/MapsAdderActivity.java
@@ -2,6 +2,7 @@ package com.grrigore.tripback_up;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,33 +11,70 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.grrigore.tripback_up.model.Place;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsAdderActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private List<Place> places;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_adder);
+
+        places = new ArrayList<>();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                mMap.clear();
+
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+
+                places.add(new Place(Double.toString(latLng.latitude), Double.toString(latLng.longitude)));
+            }
+        });
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //move the camera to the center of the map
         mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(0,0)));
     }
 
     public void addMarkerToMap(View view) {
+
     }
 
     public void saveMarkers(View view) {
+        //todo save markers to firebase database
+        for (Place place : places) {
+            Log.d("PLACES", place.getLat() + " " + place.getLng());
+        }
     }
 }
--- app/src/main/java/com/grrigore/tripback_up/model/Place.java
+++ app/src/main/java/com/grrigore/tripback_up/model/Place.java
@@ -1,24 +1,15 @@
 package com.grrigore.tripback_up.model;
 
 public class Place {
-    private Long id;
     private String lat;
     private String lng;
 
-    public Place(){}
+    public Place() {
+    }
 
-    public Place(Long id, String lat, String lng) {
-        this.id = id;
+    public Place(String lat, String lng) {
         this.lat = lat;
         this.lng = lng;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getLat() {
