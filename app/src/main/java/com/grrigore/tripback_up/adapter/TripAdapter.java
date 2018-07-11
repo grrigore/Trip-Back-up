0fb1a3b3aed2345088302ef75f506a6b9bf3f1d1
grrigore <cristi9627@yahoo.com>
2018-07-11 16:54:32 +0300

    changed adapter

--- app/src/main/java/com/grrigore/tripback_up/adapter/TripAdapter.java
+++ app/src/main/java/com/grrigore/tripback_up/adapter/TripAdapter.java
@@ -1,5 +1,6 @@
 package com.grrigore.tripback_up.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.StorageReference;
 import com.grrigore.tripback_up.R;
 import com.grrigore.tripback_up.model.Trip;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -19,9 +22,13 @@ import butterknife.ButterKnife;
 public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
 
     private List<Trip> trips;
+    private StorageReference storageReference;
+    private Context context;
 
-    public TripAdapter(List<Trip> trips) {
+    public TripAdapter(List<Trip> trips, StorageReference storageReference, Context context) {
         this.trips = trips;
+        this.storageReference = storageReference;
+        this.context = context;
     }
 
     @Override
@@ -34,7 +41,11 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         Trip trip = trips.get(position);
         holder.tvTitle.setText(trip.getTitle());
-        Picasso.get().load(trip.getPhotos().get(0)).into(holder.ivImage);
+        // Load the image using Glide
+        Glide.with(context)
+                .using(new FirebaseImageLoader())
+                .load(storageReference)
+                .into(holder.ivImage);
     }
 
     @Override
