25a2beb0de6c41ee28a0bce70f2205941f30d2d6
grrigore <cristianandreigrigore@gmail.com>
2018-07-18 17:02:49 +0300

    detail UI settings

--- app/src/main/java/com/grrigore/tripback_up/TripDetailActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/TripDetailActivity.java
@@ -2,11 +2,26 @@ package com.grrigore.tripback_up;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.MapView;
 import com.grrigore.tripback_up.model.Trip;
 
+import butterknife.BindView;
+
 public class TripDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTripTitle)
+    TextView tvTripTitle;
+    @BindView(R.id.tvTripDescription)
+    TextView tvTripDescription;
+    @BindView(R.id.rvTripGallery)
+    RecyclerView rvTripGallery;
+    @BindView(R.id.mvTripPlaces)
+    MapView mvTripPlaces;
 
     private Trip trip;
 
@@ -22,5 +37,11 @@ public class TripDetailActivity extends AppCompatActivity {
 
         Log.d(getApplicationContext().getClass().getSimpleName(), trip.toString());
 
+    }
+
+    public void markAsFavourite(View view) {
+    }
+
+    public void openDetailMap(View view) {
     }
 }
