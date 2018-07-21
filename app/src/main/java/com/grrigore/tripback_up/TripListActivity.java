6153bc0c9a71b2f638bcbf7d9ab082d2d12c91e6
grrigore <cristianandreigrigore@gmail.com>
2018-07-21 16:29:27 +0300

    dif options fail - to review

--- app/src/main/java/com/grrigore/tripback_up/TripListActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/TripListActivity.java
@@ -32,11 +32,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.grrigore.tripback_up.utils.Constants.SEVEN_DAYS_IN_MS;
 import static com.grrigore.tripback_up.utils.Constants.SHARED_PREFERENCES;
 import static com.grrigore.tripback_up.utils.Constants.TRIP_CLICKED_DESCRIPTION;
 import static com.grrigore.tripback_up.utils.Constants.TRIP_CLICKED_TITLE;
 
 //TODO on screen rotate
+//TODO think
 
 public class TripListActivity extends AppCompatActivity {
 
@@ -49,19 +51,178 @@ public class TripListActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseStorage firebaseStorage;
     private long tripId;
-    private boolean isRecentTrip = false;
+    private List<Trip> recentTrips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (isRecentTrip) {
-            viewAllTrips();
+
+        //create instance of firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //create instance of firebase storage
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        //create instance of firebase database
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            tripId = bundle.getLong("tripId");
+            Log.d(TripListActivity.class.getSimpleName(), "Trip id sent from trip adder = " + tripId);
         } else {
+            // Read from the database
+            databaseReference.child("users/" + firebaseAuth.getCurrentUser().getUid() + "/").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    tripId = (long) dataSnapshot.child("tripNumber").getValue();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TripListActivity.class.getSimpleName(), "Failed to read trip.");
+                }
+            });
+        }
+
+        recentTrips = new ArrayList<>();
+        getRecentTrips();
+
+        boolean isRecentTrip;
+        if (recentTrips.size() == 0) {
+            isRecentTrip = false;
+        } else {
+            isRecentTrip = true;
+        }
+
+        if (!isRecentTrip) {
             setContentView(R.layout.no_recent_trips_layout);
+        } else {
+            viewAllRecentTrips();
         }
     }
 
-    private void setUI() {
+    private void viewAllRecentTrips() {
+        setContentView(R.layout.activity_trip_list);
+
+        ButterKnife.bind(this);
+
+        //create instance of firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //create instance of firebase storage
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        //create instance of firebase database
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            tripId = bundle.getLong("tripId");
+            Log.d(TripListActivity.class.getSimpleName(), "Trip id sent from trip adder = " + tripId);
+        } else {
+            // Read from the database
+            databaseReference.child("users/" + firebaseAuth.getCurrentUser().getUid() + "/").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    tripId = (long) dataSnapshot.child("tripNumber").getValue();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TripListActivity.class.getSimpleName(), "Failed to read trip.");
+                }
+            });
+        }
+
+        getRecentTrips();
+    }
+
+    private void getRecentTrips() {
+        Date currentDate = new Date();
+        final long currentTime = currentDate.getTime();
+
+        databaseReference.child("users/" + firebaseAuth.getCurrentUser().getUid() + "/").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final List<Trip> tripList = new ArrayList<>();
+                final List<StorageReference> imageRefs = new ArrayList<>();
+
+                DataSnapshot tripsDataSnapshot = dataSnapshot.child("trips");
+                for (DataSnapshot tripDataSnapshot : tripsDataSnapshot.getChildren()) {
+                    Trip trip = new Trip();
+
+                    trip.setTitle((String) tripDataSnapshot.child("title").getValue());
+                    trip.setDescription((String) tripDataSnapshot.child("description").getValue());
+
+
+                    DataSnapshot dateDataSnapshot = tripDataSnapshot.child("date");
+                    Date date = new Date();
+                    if (dateDataSnapshot.child("time").getValue() != null) {
+                        date.setTime((Long) dateDataSnapshot.child("time").getValue());
+                    }
+                    trip.setDate(date);
+
+                    DataSnapshot imagesDataSnapshot = tripDataSnapshot.child("images");
+                    List<String> imageList = new ArrayList<>();
+                    for (int i = 1; i <= imagesDataSnapshot.getChildrenCount(); i++) {
+                        imageList.add(String.valueOf(imagesDataSnapshot.child("img" + i).getValue()));
+                    }
+                    trip.setImages(imageList);
+
+                    DataSnapshot placesDataSnapshot = tripDataSnapshot.child("places");
+                    List<Place> placeList = new ArrayList<>();
+                    for (int i = 0; i < placesDataSnapshot.getChildrenCount(); i++) {
+                        Place place = new Place();
+                        place.setLat((String) placesDataSnapshot.child(String.valueOf(i)).child("lat").getValue());
+                        place.setLng((String) placesDataSnapshot.child(String.valueOf(i)).child("lng").getValue());
+                        placeList.add(place);
+                    }
+                    trip.setPlaces(placeList);
+
+                    if (date.getTime() - currentTime <= SEVEN_DAYS_IN_MS) {
+                        recentTrips.add(trip);
+                    }
+
+                    //get first image form each trip
+                    imageRefs.add(firebaseStorage.getReferenceFromUrl(imageList.get(0)));
+                }
+
+                tripAdapter = new TripAdapter(recentTrips, imageRefs, getApplicationContext());
+
+                RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+                //set on item click listener
+                tripAdapter.setItemClickListener(new TripAdapter.ItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        SharedPreferences.Editor sharedPreferencesEditor = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE).edit();
+                        sharedPreferencesEditor.putString(TRIP_CLICKED_TITLE, tripList.get(position).getTitle());
+                        sharedPreferencesEditor.putString(TRIP_CLICKED_DESCRIPTION, tripList.get(position).getDescription());
+                        sharedPreferencesEditor.apply();
+
+                        Intent tripDetailIntent = new Intent(TripListActivity.this, TripDetailActivity.class);
+                        tripDetailIntent.putExtra("tripClicked", tripList.get(position));
+                        tripDetailIntent.putExtra("tripId", position + 1);
+                        tripDetailIntent.putExtra("userUID", firebaseAuth.getCurrentUser().getUid());
+                        startActivity(tripDetailIntent);
+                    }
+                });
+                rlvTrips.setLayoutManager(layoutManager);
+                rlvTrips.setItemAnimator(new DefaultItemAnimator());
+                rlvTrips.setAdapter(tripAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("-----Error-----", databaseError.getMessage());
+            }
+        });
+    }
+
+    private void getAllTrips() {
         databaseReference.child("users/" + firebaseAuth.getCurrentUser().getUid() + "/").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -138,7 +299,6 @@ public class TripListActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
@@ -152,9 +312,11 @@ public class TripListActivity extends AppCompatActivity {
         switch (id) {
             case R.id.recentTrips:
                 //todo recent trip selection
+                viewAllRecentTrips();
                 return true;
             case R.id.pastTrips:
                 //todo past trip selection
+                viewAllTrips();
                 return true;
             case R.id.favTrips:
                 //todo fav trip selection
@@ -171,7 +333,7 @@ public class TripListActivity extends AppCompatActivity {
         }
     }
 
-    public void setAllTripsMode(View view) {
+    public void allTripsMode(View view) {
         viewAllTrips();
     }
 
@@ -209,6 +371,6 @@ public class TripListActivity extends AppCompatActivity {
             });
         }
 
-        setUI();
+        getAllTrips();
     }
 }
