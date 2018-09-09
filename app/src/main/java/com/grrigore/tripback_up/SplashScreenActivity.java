ed904767c042fa52c7b060681b4ce22451d7d12a
grrigore <cristianandreigrigore@gmail.com>
2018-09-09 16:22:30 +0300

    added simle animation

--- app/src/main/java/com/grrigore/tripback_up/SplashScreenActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/SplashScreenActivity.java
@@ -4,22 +4,41 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    //todo move into constants class
+    public static final int ANIMATION_OFFSET = 60;
+
+    @BindView(R.id.ivProgress)
+    ImageView ivProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //hide toolbar
-        getSupportActionBar().hide();
+        ButterKnife.bind(this);
 
+        //hide toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        imageTranslation();
         new SplashScreenAsyncTask().execute();
 
     }
 
     private class SplashScreenAsyncTask extends AsyncTask<Void, Void, Void> {
+
         @Override
         protected Void doInBackground(Void... voids) {
             try {
@@ -38,5 +57,22 @@ public class SplashScreenActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+
+    }
+
+    private void imageTranslation() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        float width = displayMetrics.widthPixels;
+
+        float xCurrentPos, yCurrentPos;
+        xCurrentPos = ivProgress.getLeft();
+        yCurrentPos = ivProgress.getTop();
+
+        Animation animation = new TranslateAnimation(xCurrentPos - ANIMATION_OFFSET, xCurrentPos + width, yCurrentPos, yCurrentPos);
+        animation.setDuration(5000);
+        animation.setFillAfter(true);
+        animation.setFillBefore(true);
+        ivProgress.startAnimation(animation);
     }
 }
