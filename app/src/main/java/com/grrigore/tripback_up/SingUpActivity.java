21d504d4f118a04c5938abfe10785f1625f9b19e
grrigore <cristianandreigrigore@gmail.com>
2018-09-09 14:37:48 +0300

    minor warning

--- app/src/main/java/com/grrigore/tripback_up/SingUpActivity.java
+++ app/src/main/java/com/grrigore/tripback_up/SingUpActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -43,7 +42,9 @@ public class SingUpActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         //hide toolbar
-        getSupportActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         //get firebase auth instance
         firebaseAuth = FirebaseAuth.getInstance();
