1ce620181364b8ac01a24e0a5e47d2d8bad2464f
grrigore <cristianandreigrigore@gmail.com>
2018-10-22 14:46:39 +0300

    Added trip editor activity.

---
+++ app/src/main/java/com/grrigore/tripback_up/TripEditorActivity.java
@@ -0,0 +1,23 @@
+package com.grrigore.tripback_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TripEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_trip);
+    }
+
+    public void addMedia(View view) {
+    }
+
+    public void addPlace(View view) {
+    }
+
+    public void saveTrip(View view) {
+    }
+}
